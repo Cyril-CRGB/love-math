64af640eb60407c042bac7744e4fdf164d0f48ee
Cyril-CRGB <>
2023-06-06 05:51:08 +0000

    Add code to calculate two random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert(`You clicked ${gameType}`);
             }
         });
-
     }
 });
+/**
+ * The main game "loop", called when the script is first loaded
+ * and after the user's answer has been processed
+ */
+function runGame() {
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+}
 
-function runGame{ }
-function checkAnswer{ }
-function calculateCorrectAnswer{ }
-function incrementScore{ }
-function incrementWrongAnswer{ }
-function displayAdditionQuestion{ }
-function displaySubtractQuestion{ }
-function displayMultiplyQuestion{ }
-function displayDivisionQuestion{ };
+function checkAnswer() { }
+function calculateCorrectAnswer() { }
+function incrementScore() { }
+function incrementWrongAnswer() { }
+function displayAdditionQuestion() { }
+function displaySubtractQuestion() { }
+function displayMultiplyQuestion() { }
+function displayDivisionQuestion() { }
